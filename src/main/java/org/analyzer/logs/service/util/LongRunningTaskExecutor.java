0210a1d28dd21935f00e63f45dbfb8261a10f098
Alik <typicaljprogrammer@gmail.com>
2023-01-24 01:23:05 +0300

    Metrics for long running tasks

--- src/main/java/org/analyzer/logs/service/util/LongRunningTaskExecutor.java
+++ src/main/java/org/analyzer/logs/service/util/LongRunningTaskExecutor.java
@@ -1,7 +1,11 @@
 package org.analyzer.logs.service.util;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.annotation.PreDestroy;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.scheduler.Scheduler;
@@ -11,14 +15,28 @@ import reactor.core.scheduler.Schedulers;
 public class LongRunningTaskExecutor {
 
     private final Scheduler scheduler;
+    private final Counter longRunningTasksCounter;
+    private final Timer timer;
 
-    public LongRunningTaskExecutor(@Value("${logs.task.executor.pool-size:4}") final int poolSize) {
+    @Autowired
+    public LongRunningTaskExecutor(
+            @Value("${logs.task.executor.pool-size:4}") final int poolSize,
+            @NonNull final MeterRegistry meterRegistry) {
         this.scheduler = Schedulers.newParallel("long-running-tasks-scheduler", poolSize);
         this.scheduler.init();
+
+        this.longRunningTasksCounter = Counter.builder("long-running-tasks")
+                                                .description("All scheduled long running tasks")
+                                                .register(meterRegistry);
+        this.timer = Timer.builder("long-running-tasks-timer")
+                            .description("Long running tasks timer")
+                            .publishPercentileHistogram()
+                            .register(meterRegistry);
     }
 
     public void execute(@NonNull final Runnable task) {
-        this.scheduler.schedule(task);
+        this.longRunningTasksCounter.increment();
+        this.scheduler.schedule(this.timer.wrap(task));
     }
 
     @PreDestroy
